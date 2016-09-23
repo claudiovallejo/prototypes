201459557cbfc7ddd7065d0558d249fa32a3a71c
Claudio Vallejo <hola@claudiovallejo.mx>
2016-09-23 16:54:59 -0500

    Added relative links and directory indexing. Cleaned up unused configurations.

--- config.rb
+++ config.rb
@@ -16,29 +16,13 @@ page '/*.txt', layout: false
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
 #  which_fake_page: "Rendering a fake page with a local variable" }
 
+# Relative Links & Directory Indexing
+activate :directory_indexes
+set :relative_links, true
+
 # General configuration
 
 # Reload the browser automatically whenever files change
 configure :development do
   activate :livereload
-end
-
-###
-# Helpers
-###
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
-# Build-specific configuration
-configure :build do
-  # Minify CSS on build
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
 end
