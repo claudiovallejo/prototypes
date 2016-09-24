3a4ed25b2a1ce25786309e30512066b31a419ad0
Claudio Vallejo <hola@claudiovallejo.mx>
2016-09-24 18:03:39 -0500

    Cleaned up unused configurations. Created proxy pages for project details.

--- config.rb
+++ config.rb
@@ -1,26 +1,20 @@
 ###
-# Page options, layouts, aliases and proxies
+# Site Configurations
 ###
 
-# Per-page layout changes:
-#
-# With no layout
+# Pages with no layout
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
+# Proxies
+data.projects.each do |project|
+  proxy "/projects/#{ I18n.transliterate(project.name).downcase.strip.gsub(' ', '-') }/index.html", "/projects/detail.html", :locals => { :project => project }
+end
 
 # Relative Links & Directory Indexing
 activate :directory_indexes
 set :relative_links, true
-
-# General configuration
 
 # Reload the browser automatically whenever files change
 configure :development do
