ecdb370905ba247774d52e75f2b07161ab12bc2b
Claudio Vallejo <hola@claudiovallejo.mx>
2016-09-25 20:51:38 -0500

    Finished basic, working prototype for first design exploration.

--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -7,4 +7,33 @@ module CustomHelpers
   def pnum cel
     number = cel.gsub(' ', '').gsub('(', '').gsub(')', '')
   end
+  # Project Index: fetches project index value
+  def project_index name
+    i = 0
+    data.projects.each do |project|
+      break if name === project.name
+      if name != project.name
+        i = i + 1
+      end
+    end
+    i
+  end
+  # Other Projects: fetches the next two projects from the project database
+  def other_projects i
+    others = []
+    projects = data.projects
+    a = ""
+    b = ""
+    if i === projects.length - 2
+      a = projects[projects.length]
+      b = projects[0]
+    elsif i === projects.length - 1
+      a = projects[0]
+      b = projects[1]
+    else
+      a = projects[i + 1]
+      b = projects[i + 2]
+    end
+    others += [a, b]
+  end
 end
--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -40,21 +40,58 @@ function toggle() {
 }
 /* - - - - - - - - - */
 const $filters = document.getElementsByClassName('filter');
+const $container = document.getElementById('projects');
+//
 for (var i = 0; i < $filters.length; i ++) {
   $filters[i].addEventListener('click', function(e){
     e.preventDefault();
-    console.log(this.innerHTML);
-    filterToggle(this);
+    styleFilterTabs(this);
+    updateProjectList(this);
   });
 }
-
-function filterToggle($el) {
+//
+function styleFilterTabs($el) {
   for (var i = 0; i < $filters.length; i++) {
     $filters[i].classList.remove('dark-gray');
     $filters[i].classList.add('moon-gray');
   }
   $el.classList.remove('moon-gray');
   $el.classList.add('dark-gray');
+}
+//
+function updateProjectList($el) {
+  //
+  selectedCategory = $el.innerHTML;
+  //
+  notActive = true;
+  //
+  if (notActive) {
+    $container.classList.remove('fi');
+    $container.classList.add('fo');
+    setTimeout(function(){
+      hideProjects();
+      $container.classList.remove('fo');
+      $container.classList.remove('fi');
+    }, 455);
+  }
+
+}
+//
+function hideProjects() {
+  for (var i = 0; i < $container.children.length; i++) {
+    //
+    if (selectedCategory === "All") {
+      $container.children[i].classList.remove('dn');
+    }
+    //
+    else if ($container.children[i].dataset.category !== selectedCategory) {
+      $container.children[i].classList.add('dn');
+    }
+    //
+    else {
+      $container.children[i].classList.remove('dn');
+    }
+  }
 }
 /* - - - - - - - - - */
 const $content = document.getElementById('content');
