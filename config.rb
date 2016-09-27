0e9bae6b4dcdb547f02c6285f73a611a4b6c9941
Claudio Vallejo <hola@claudiovallejo.mx>
2016-09-27 16:56:05 -0500

    Modified config.rb: Added proxies for Prototype #2 project details.

--- config.rb
+++ config.rb
@@ -7,9 +7,18 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
+###
 # Proxies
+###
+
+# Prototype #1 Project Details
 data.projects.each do |project|
   proxy "prototype1/projects/#{ I18n.transliterate(project.name).downcase.strip.gsub(' ', '-') }/index.html", "prototype1/projects/detail.html", :locals => { :project => project }, :ignore => true
+end
+
+# Prototype #2 Project Details
+data.projects2.each do |project|
+  proxy "prototype2/projects/#{ I18n.transliterate(project.name).downcase.strip.gsub(' ', '-') }/index.html", "prototype2/projects/detail.html", :locals => { :project => project }, :ignore => true
 end
 
 # Relative Links & Directory Indexing
