b4325cdf0b19480f4274e0bd1cf1ada77d760c9b
Claudio Vallejo <hola@claudiovallejo.mx>
2016-09-27 08:51:03 -0500

    Modified : Updated classList.toggle classes.

--- source/javascripts/home.js
+++ source/javascripts/home.js
@@ -7,7 +7,11 @@
 */
 const $tab = document.getElementById('tab');
 const $arrow = document.getElementById('arrow');
+const $main_menu = document.getElementById('main-menu');
+//  Click Event Listener
 $tab.addEventListener('click', function(){
   console.log("Click");
-  $arrow.classList.toggle('rotate');
+  $arrow.classList.toggle('tr-180');
+  $tab.classList.toggle('tty-84');
+  $main_menu.classList.toggle('tty-84');
 });
