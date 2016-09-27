8faf6d4998d7c6e499bf926a1a980afc0041b9c6
Claudio Vallejo <hola@claudiovallejo.mx>
2016-09-27 13:36:25 -0500

    Modified : Added comments.

--- source/javascripts/home.js
+++ source/javascripts/home.js
@@ -5,13 +5,16 @@
   Adds interactivity to the bottom tab, which shows/hides the navigation links for the prototype website.
 
 */
+//  Caching menu tab, menu arrow, and navigation banner
 const $tab = document.getElementById('tab');
 const $arrow = document.getElementById('arrow');
 const $main_menu = document.getElementById('main-menu');
 //  Click Event Listener
 $tab.addEventListener('click', function(){
-  console.log("Click");
+  //  Toggle `transform: rotate(180deg);`
   $arrow.classList.toggle('tr-180');
-  $tab.classList.toggle('tty-84');
-  $main_menu.classList.toggle('tty-84');
+  //  Toggle `transform: translateY(-84px);`
+  $tab.classList.toggle('tty--84');
+  //  Toggle `transform: translateY(-84px);`
+  $main_menu.classList.toggle('tty--84');
 });
